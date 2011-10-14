9675b5672bae1c23ff8b355022a3dc5f2e3b9567
Germano Nicolini <germanogmn1@gmail.com>
2011-10-14 13:19:08 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Presentear::Application.config.secret_token = '55fd0da793b17639b5ddb67a635d85f9c8499184f865d59e3e11e00572fa31aaff4ea5acc22b9e5f8f755a8ecc1c99878f6859f57d12a1b5e76ee179b40cdd9d'
