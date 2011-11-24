07042a25e8fdc8a0f66460281bbb8bc487cec175
NicholasPufal <niko.pufal@gmail.com>
2011-11-24 14:38:11 -0200

    It seems layouts are finished, except for the 404 and "event expired".

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def link_to_if_present(link, name, options = {})
+    
+    if link.present?
+      link_to(name, link, options) 
+    else
+      name
+    end
+  
+  end
 end
