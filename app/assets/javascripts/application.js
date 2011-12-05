9fe1274c0eb03fc00e7b4296e1865e6ca860fd75
NicholasPufal <niko.pufal@gmail.com>
2011-12-05 17:24:07 -0200

    Fix for the placeholder. Now it fully uses Javascript.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,6 @@
 //= require fancybox
 //= require_tree .
 
-
 $(document).ready(function() {
 	
 	$("a#add_suggestion").fancybox();
@@ -21,12 +20,39 @@ $(document).ready(function() {
 	
 	$('input[placeholder], textarea[placeholder]').placeholder();
 	
+	//Tooltip
 	$("input#event_input")
 		.focus(function() { 
 			$("span#event_helper").fadeIn();
 		})
 		.blur(function() { 
 			$("span#event_helper").fadeOut();
+		})	
+		
+	//Simulate placeholder behavior -> Was buggy to set text-align:center to a placeholder in Safari...
+	
+	//Prevent form from considerating the placeholder as a valid value
+	$("form#new_event").submit(function() {
+		$("input[data-placeholder]").each(function() {
+			if ($(this).val() == $(this).attr("data-placeholder")) 
+				$(this).val("")
+		})
+	})
+	
+	//Set data-placeholder as the default value
+	$("input[data-placeholder]").each(function() {
+		$(this).val($(this).attr("data-placeholder"));
+	})
+	
+	//Handle placeholder in/out
+	$("input[data-placeholder]")
+		.focus(function() {
+			if ($(this).val() === $(this).attr("data-placeholder"))
+				$(this).val("");
+		})
+		.blur(function() {
+			if ($(this).val() === "")
+				$(this).val($(this).attr("data-placeholder"));
 		})
 	
 });
